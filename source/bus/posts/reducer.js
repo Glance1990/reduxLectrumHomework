6a81eb42953668198baf20c231b5b778667000a0
Glance1990 <paulkadysh@gmail.com>
2019-04-06 16:02:56 +0300

    practise lesson 3

---
+++ source/bus/posts/reducer.js
@@ -0,0 +1,22 @@
+// Core
+import { fromJS, List } from 'immutable';
+
+// Instruments
+import { FILL_POSTS } from './types';
+
+const initialState = List();
+
+const list1 = List([1, 2, 3]);
+const list2 = list1.push(4);
+
+console.log(list1, list2);
+
+export const postsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FILL_POSTS:
+            return fromJS(action.payload);
+
+        default: 
+            return state;
+    }
+}
