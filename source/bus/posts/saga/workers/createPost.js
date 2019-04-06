417165658c0015d0877fd4980d68386108ee5c79
Glance1990 <paulkadysh@gmail.com>
2019-04-06 20:37:20 +0300

    end of video practice

--- source/bus/posts/saga/workers/createPost.js
+++ source/bus/posts/saga/workers/createPost.js
@@ -3,14 +3,28 @@ import { put, apply } from 'redux-saga/effects';
 
 // Instruments
 import { api } from '../../../../REST';
-import { createPost as createPostAC } from '../../actions';
+import { postsActions } from '../../actions';
+import { uiActions } from '../../../ui/actions';
 
 export function* createPost({ payload: comment }) {
 
-    const response = yield apply(api, api.posts.create, [comment]);
-    //const response = yield call(api.posts.create(comment );
+	try {
+		yield put(uiActions.startFetching());
 
-    const result = yield apply(response, response.json());
+		const response = yield apply(api, api.posts.create, [comment]);
 
-    yield put(createPostAC(result.data));
+	    const { data: post, message } = yield apply( response, response.json );
+
+	    if ( response.status !== 200 ) {
+	    	throw new Error(message);
+	    }
+
+    	yield put(postsActions.createPost(post));
+
+	} catch (error) {
+		yield put(uiActions.emitError(error, 'createPost worker'));
+	} finally {
+		yield put(uiActions.stopFetching());
+	}
+    
 }
